e5aab74d3d925c877e039b82a8221c83e67172ab
蒋远路 <luseike.jiang@wattsonic.com>
2024-10-19 12:00:51 +0800

    Initial Commit

---
+++ Birthdays/BirthdaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  BirthdaysApp.swift
+//  Birthdays
+//
+//  Created by 远路蒋 on 2024/10/19.
+//
+
+import SwiftUI
+
+@main
+struct BirthdaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
